6e986d197516923e0ff39c7a2014a0d5743e0b24
rizzfar <alfaridrizky69@gmail.com>
2025-01-04 20:59:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loader = document.getElementById('loader');
+  const appContainer = document.getElementById('app-container');
+  document.body.style.overflowY = 'hidden';
+  
+  setTimeout(() => {
+    loader.style.display = 'none';
+    appContainer.style.display = 'block';
+    document.body.style.overflowY = 'auto';
+  }, 2000)
+})
