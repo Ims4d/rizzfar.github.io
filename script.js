b3659f33adde32038498b5d94a9cdf600e491781
rizzfar <alfaridrizky69@gmail.com>
2025-01-05 09:43:11 +0700

    first commit

--- script.js
+++ script.js
@@ -2,14 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
   const loader = document.getElementById('loader');
   const appContainer = document.getElementById('app-container');
   const imageProfile = document.getElementById('image-profile');
+  const toggleButton = document.getElementById('dark-mode-toggle');
+  const darkModeIcon = document.getElementById('dark-mode-icon');
+  const body = document.body;
 
   document.body.style.overflowY = 'hidden';
   imageProfile.style.display = 'none';
+  toggleButton.style.display = 'none';
   
   setTimeout(() => {
     loader.style.display = 'none';
     appContainer.style.display = 'block';
     document.body.style.overflowY = 'auto';
     imageProfile.style.display = 'block';
-  }, 3000)
+    toggleButton.style.display = 'block';
+  }, 4000);
+
+  toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+  
+    if (body.classList.contains('dark-mode')) {
+      darkModeIcon.classList.remove('fa-sun');
+      darkModeIcon.classList.add('fa-moon');
+    } else {
+      darkModeIcon.classList.remove('fa-moon');
+      darkModeIcon.classList.add('fa-sun');
+    }
+  });
+  
 })
